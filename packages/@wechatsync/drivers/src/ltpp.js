83e46999f63a014189864e1671239b036bd4c222
尤雨东 <root@ltpp.vip>
2025-06-07 22:39:45 +0800

    update: code

--- packages/@wechatsync/drivers/src/ltpp.js
+++ packages/@wechatsync/drivers/src/ltpp.js
@@ -40,38 +40,4 @@ export const LTPP = `<div data-v-7a63e4b3="" class="v-show-content scroll-style 
 </blockquote>
 <p><img src="https://picx.zhimg.com/80/v2-d2e748203d2cb238477029e349f92877_1440w.png" alt="" referrerpolicy="no-referrer"></p>
 <p><img src="https://pica.zhimg.com/80/v2-b71c69403e5aa2fbcac9a414fa9dbe2e_1440w.png" alt="" referrerpolicy="no-referrer"></p>
-<h3><a id="WEBhttpsdocsltppvipLTPPWEBIDEhttpsdocsltppvipLTPPWEBIDE_53"></a>免费在线WEB代码编辑器（<a href="https://docs.ltpp.vip/LTPP-WEB-IDE/" target="_blank">https://docs.ltpp.vip/LTPP-WEB-IDE/</a>）</h3>
-<blockquote>
-<p>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 无论你是开发者还是学生，编程环境的搭建和管理可能会占用你宝贵的时间和精力。现在，有一款强大的免费在线代码编辑器，支持多种编程语言，让您可以随时随地编写和运行代码，提升编程效率，专注于创意和开发。
-<br><b>多语言支持，无缝切换</b><br>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 这款在线代码编辑器支持包括C、C++、JavaScript、TypeScript、Go、Rust、PHP、Java、Ruby、Python3以及C#在内的多种编程语言，无论您的项目需要哪种语言，都能在这里找到支持。
-<br><b>在线运行，快速定位问题</b><br>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 您可以在编写代码的同时，即时运行并查看结果，快速定位并解决问题，提高开发效率。
-<br><b>代码高亮与智能提示</b><br>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 编辑器提供代码高亮和智能提示功能，帮助您更快地编写代码，减少错误，提升编码质量。
-</p>
-</blockquote>
-<p><img src="https://picx.zhimg.com/80/v2-ce64b6b5ebf19ebfdf410283b5dce3f1_1440w.png" alt="" referrerpolicy="no-referrer"></p>
-<p><img src="https://picx.zhimg.com/80/v2-c51d0fbaccc6e355601e49a2fdfd1262_1440w.png" alt="" referrerpolicy="no-referrer"></p>
-<h3><a id="httpsdocsltppvipLTPPQRCODEhttpsdocsltppvipLTPPQRCODE_71"></a>免费二维码生成器（<a href="https://docs.ltpp.vip/LTPP-QRCODE/" target="_blank">https://docs.ltpp.vip/LTPP-QRCODE/</a>）</h3>
-<blockquote>
-<p>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 二维码（QR Code）是一种二维条码，能够存储更多信息，并且可以通过智能手机等设备快速扫描识别。它广泛应用于各种场景，如：
-<br><b>企业宣传</b><br>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 企业可以通过二维码分享公司网站、产品信息、服务介绍等。
-<br><b>活动推广</b><br>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 活动组织者可以创建二维码，参与者扫描后可以直接访问活动详情、报名链接或获取电子门票。
-<br><b>个人信息分享</b><br>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 个人可以生成包含联系方式、社交媒体链接、个人简历等信息的二维码。
-<br><b>电子商务</b><br>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 商家使用二维码进行商品追踪、促销活动、在线支付等。
-<br><b>教育</b><br>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 教师可以创建二维码，学生扫描后可以直接访问学习资料或在线课程。
-<br><b>交通出行</b><br>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 二维码用于公共交通的票务系统，乘客扫描二维码即可进出站或支付车费。
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 功能强大的二维码生成器通常具备用户界面友好，操作简单，即使是初学者也能快速上手和生成的二维码可以在各种设备和操作系统上扫描识别的特点。
-</p>
-</blockquote>
-<p><img src="https://picx.zhimg.com/80/v2-29354abe4e9e3bdac76d974e0910fea3_1440w.png" alt="" referrerpolicy="no-referrer"></p>
 </div>`
